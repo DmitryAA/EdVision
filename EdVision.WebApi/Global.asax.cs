949d24e9a7fc404085bc17ec1568d22dc746274b
ROKarpov <karpovro@gmail.com>
2019-06-23 13:34:01 +0300

    Fix global

--- EdVision.WebApi/Global.asax.cs
+++ EdVision.WebApi/Global.asax.cs
@@ -13,9 +13,6 @@ namespace EdVision
     {
         protected void Application_Start() {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
