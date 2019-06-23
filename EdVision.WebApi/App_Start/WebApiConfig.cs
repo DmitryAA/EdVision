a64b524dc4361c7f560b7b50d25042bc714530ee
ROKarpov <karpovro@gmail.com>
2019-06-23 03:52:17 +0300

    Fix API

--- EdVision.WebApi/App_Start/WebApiConfig.cs
+++ EdVision.WebApi/App_Start/WebApiConfig.cs
@@ -19,6 +19,12 @@ namespace EdVision
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Routes.MapHttpRoute(
+               name: "ActionableApi",
+               routeTemplate: "api/{controller}/{action}/{id}",
+               defaults: new { id = RouteParameter.Optional }
+           );
         }
     }
 }
